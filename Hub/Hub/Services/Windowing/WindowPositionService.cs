f10967e20d370e11ec2866b24beba7b7019d3a00
plaskarzewski <pawel.laskarzewski@auctane.com>
2026-09-24 11:07:32 +0200

    Fix window sizing

--- Hub/Hub/Services/Windowing/WindowPositionService.cs
+++ Hub/Hub/Services/Windowing/WindowPositionService.cs
@@ -1,17 +1,29 @@
 using System.Windows;
 using System.Windows.Forms;
+using System.Windows.Media;
 
 namespace Hub.Services.Windowing;
 
 public static class WindowPositionService
 {
+    private const double MaxHeightScreenPercentage = 0.5;
+
     public static void CenterOnCurrentMonitor(Window window)
     {
         var cursorPosition = Cursor.Position;
         var screen = Screen.FromPoint(cursorPosition);
         var area = screen.WorkingArea;
 
-        window.Left = area.Left + (area.Width - window.Width) / 2;
-        window.Top = area.Top + (area.Height - window.Height) / 2;
+        var dpiScale = VisualTreeHelper.GetDpi(window);
+
+        var areaLeft = area.Left / dpiScale.DpiScaleX;
+        var areaTop = area.Top / dpiScale.DpiScaleY;
+        var areaWidth = area.Width / dpiScale.DpiScaleX;
+        var areaHeight = area.Height / dpiScale.DpiScaleY;
+
+        window.MaxHeight = areaHeight * MaxHeightScreenPercentage;
+
+        window.Left = areaLeft + (areaWidth - window.Width) / 2;
+        window.Top = areaTop + (areaHeight - window.Height) / 2;
     }
 }
